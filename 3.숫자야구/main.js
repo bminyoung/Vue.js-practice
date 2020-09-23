971ffe39a5747930936d7f1bc42063330ca59815
DESKTOP-AM12U9O\MinYoung <qo613@naver.com>
2020-09-24 01:04:44 +0900

    webpack ex

--- 3.숫자야구/main.js
+++ 3.숫자야구/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import NumberBaseball from './NumberBaseball';
+//import NumberBaseball from './NumberBaseball.vue';인데 resolve의 extension 설정때문에 알아서함
 
 //Vue의 el 역할
-new Vue().$mount('#root');
+new Vue(NumberBaseball).$mount('#root');
