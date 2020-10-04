b2c0a58607190aa8682e745634ebe7a5e92ada55
DESKTOP-AM12U9O\MinYoung <qo613@naver.com>
2020-10-04 23:11:39 +0900

    v-bind & scoped

--- 4.반응속도체크/main.js
+++ 4.반응속도체크/main.js
@@ -1,3 +1,4 @@
 import Vue from 'vue';
 import ResponseCheck from './ResponseCheck';
+
 new Vue(ResponseCheck).$mount('#root');
--- 4.반응속도체크/webpack.config.js
+++ 4.반응속도체크/webpack.config.js
@@ -2,31 +2,37 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'eval',
-    resolve: {
-        extensions: ['.js', '.vue'],
-    },
-    entry: {
-        app: path.join(__dirname, 'main.js'),
-    },
-    module: {
-        rules: [{
-            test: /\.vue$/,
-            loader: 'vue-loader',
-        },{
-            test: /\.css$/,
-            use: [
-                'vue-style-loader',
-                'css-loader',
-            ],
-        }],
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-    ],
-    output: {
-        filename: 'app.js',
-        path: path.join(__dirname, 'dist'),
-    },
+  mode: 'development',
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
+  entry: {
+    app: path.join(__dirname, 'main.js'),
+  },
+  module: { //js가 아닌것들을 js로 만들어준다
+    rules: [{
+      test: /\.vue$/,
+      use: 'vue-loader',
+    },{
+      test: /\.css$/,
+      use: [
+        'vue-style-loader',
+        { //css-loader 안먹힐때
+          loader: 'css-loader',
+          options: {
+            esModule: false,
+          }
+        }
+        
+      ]
+    }],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist'),
+  },
 };
