08c81a88e459b8a965f6ee07cf82d4e8ac041ae7
DESKTOP-AM12U9O\MinYoung <qo613@naver.com>
2020-10-08 23:39:49 +0900

    :class & :style

--- 5.가위바위보/RockScissorsPaper.vue
+++ 5.가위바위보/RockScissorsPaper.vue
@@ -1,3 +1,52 @@
-import ResponseChekct from '../';
+<template>
+    <div>
+        <div id="computer" :style="computedStyleObject"></div>
+        <div>
+            <button @click="onClickButton('바위')">바위</button>
+            <button @click="onClickButton('가위')">가위</button>
+            <button @click="onClickButton('보')">보</button>
+        </div>
+        <div>{{result}}</div>
+        <div>현재 {{score}}점</div>
+    </div>
+</template>
 
-new Vue(ResponseCheck).$mount*'#root');
+<script>
+const rspCoords = {
+    바위: '0',
+    가위: '-142px',
+    보: '-284px',
+}
+
+export default{
+    data(){
+        return {
+            imgCoord: rspCoords.바위,
+            result: '',
+            score: 0,
+        }
+    },
+    computed:{
+        computedStyleObject(){
+            return{
+                background: 'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0',
+            }
+        }
+    },
+    methods:{
+        onClickButton(choice){
+            // if(choice === '바위'){
+                
+            // }
+        },
+    },
+}
+</script>
+
+<style scoped>
+    #computer{
+        width: 142px;
+        height: 200px;
+        /* bacground-position: 0 0x; */
+    }
+</style>
