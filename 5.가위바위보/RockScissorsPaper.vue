2e697272dc6121b76171532cfcd561d2cce5cf87
DESKTOP-AM12U9O\MinYoung <qo613@naver.com>
2020-10-10 23:18:53 +0900

    complete rockscissorspaper

--- 5.가위바위보/RockScissorsPaper.vue
+++ 5.가위바위보/RockScissorsPaper.vue
@@ -8,7 +8,6 @@
         </div>
         <div>{{result}}</div>
         <div>현재 {{score}}점</div>
-        <lifecycle-example v-if="true" />
     </div>
 </template>
 
@@ -18,6 +17,21 @@
         가위: '-142px',
         보: '-284px',
     };
+
+    const scores = {
+        가위: 1,
+        바위: 0,
+        보: -1,
+    }
+
+    //가위,바위,보 중 하나 return
+    const computerChoice = (imgCoord) => {
+        return Object.entries(rspCoords).find(function (v){
+            return v[1] === imgCoord;
+        })[0];
+    }
+
+    let interval = null;
 
 export default{
     data(){
@@ -35,10 +49,35 @@ export default{
         }
     },
     methods:{
+        changeHand(){
+            interval = setInterval(() => {
+                if(this.imgCoord === rspCoords.바위){
+                    this.imgCoord = rspCoords.가위;
+                }else if(this.imgCoord === rspCoords.가위){
+                    this.imgCoord = rspCoords.보;
+                }else if(this.imgCoord === rspCoords.보){
+                    this.imgCoord = rspCoords.바위;
+                }
+            }, 100);
+        },
+
         onClickButton(choice){
-            // if(choice === '바위'){
-                
-            // }
+            clearInterval(interval);
+            const myScore = scores[choice];
+            const cpuScore = scores[computerChoice(this.imgCoord)];
+            const diff = myScore - cpuScore;
+            if(diff === 0){
+                this.result = '비겼습니다';
+            }else if([-1, 2].includes(diff)){
+                this.result = '이겼습니다';
+                this.score += 1;
+            }else{
+                this.result = '졌습니다.';
+                this.score -= 1;
+            }
+            setTimeout(() => {
+                this.changeHand();
+            }, 1000);
         },
     },
 
@@ -46,18 +85,34 @@ export default{
         console.log('beforeCreated');
     },
 
+    //보여지는데 화면에 나타나기 전
     created(){
         console.log('created');
     },
 
-    mounted(){
-        console.log('mounted');
+    beforeMount(){
+        console.log('beforeMount');
     },
 
+    //화면에 나타난 후
+    //화면 조작은 mounted에서
+    mounted(){
+        console.log('mounted');
+        this.changeHand();
+    },
+
+    //데이터가 바뀌어서 화면도 바뀔 때
     updated(){
         console.log('updated');
     },
 
+    beforeDestroy(){
+        console.log('beforeDestroy');
+        //setInterval을 멈추고 memory leak 방지
+        clearInterval(interval);
+    },
+
+    //component가 없어질때
     destroyed(){
         // console.log('created');
     }
@@ -68,6 +123,6 @@ export default{
     #computer{
         width: 142px;
         height: 200px;
-        /* bacground-position: 0 0x; */
+        background-position: 0 0;
     }
 </style>
